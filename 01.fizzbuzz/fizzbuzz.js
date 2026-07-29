28f83c933349089c0ec4119211cb3c29de39fa17
maco_green <version.k0824@gmail.com>
2026-07-29 11:53:21 +0900

    変数名をcntからnumberに変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,11 @@
-for (let cnt = 1; cnt <= 20; cnt++) {
-  if (cnt % 3 === 0 && cnt % 5 === 0) {
+for (let number = 1; number <= 20; number++) {
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (cnt % 5 === 0) {
+  } else if (number % 5 === 0) {
     console.log("Buzz");
-  } else if (cnt % 3 === 0) {
+  } else if (number % 3 === 0) {
     console.log("Fizz");
   } else {
-    console.log(String(cnt));
+    console.log(String(number));
   }
 }
